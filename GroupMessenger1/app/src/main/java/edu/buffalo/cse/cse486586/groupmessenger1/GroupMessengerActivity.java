a586966c6c217e0f9bd1802fcd626ff5e579de03
Anirudh Yellapragada <anirudhyellapragada@gmail.com>
2017-09-19 21:21:02 -0400

    Update GroupMessengerActivity.java

--- GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
+++ GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -7,6 +7,20 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.widget.TextView;
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -24,46 +38,40 @@ import java.net.UnknownHostException;
 
 import edu.buffalo.cse.cse486586.groupmessenger1.R;
 
-import android.app.Activity;
-import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-import android.util.StringBuilderPrinter;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnKeyListener;
-import android.widget.EditText;
-import android.widget.TextView;
-/**
- * GroupMessengerActivity is the main Activity for the assignment.
- * 
- * @author stevko
- *
- */
+
+// GroupMessengerActivity is the main Activity.
 public class GroupMessengerActivity extends Activity {
     static final String TAG = GroupMessengerActivity.class.getSimpleName();
+    
+    // Fixed redirection ports for 5 AVD's
     static final String REMOTE_PORT0 = "11108";
     static final String REMOTE_PORT1 = "11112";
     static final String REMOTE_PORT2 = "11116";
     static final String REMOTE_PORT3 = "11120";
     static final String REMOTE_PORT4 = "11124";
+    
+    // Fields to store key:message ID and value:message
     private static final String KEY_FIELD = "key";
     private static final String VALUE_FIELD = "value";
+    
+    // Message ID represented as a sequence number
     private static int sequenceNum = 0;
+    
+    // Server socket listening port
     static final int SERVER_PORT = 10000;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_messenger);
+        
         // Getting the port number of myself(AVD)
         TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
         String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
         final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
 
         // As we did in the PA1, lets create a server asynTask and client asyncTask
+        // Server task invocation
         try {
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
@@ -71,26 +79,23 @@ public class GroupMessengerActivity extends Activity {
             Log.e(TAG, "Error in creating a socket");
             return;
         }
-        /*
-         * TODO: Use the TextView to display your messages. Though there is no grading component
-         * on how you display the messages, if you implement it, it'll make your debugging easier.
-         */
+        
+         // Using the TextView to display messages to make debugging easier.
         TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
         
-        /*
-         * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
-         * OnPTestClickListener demonstrates how to access a ContentProvider.
-         */
+        
+        // Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
+        // OnPTestClickListener demonstrates how to access a ContentProvider. 
         findViewById(R.id.button1).setOnClickListener(
                 new OnPTestClickListener(tv, getContentResolver()));
 
-        /*
-         * TODO: You need to register and implement an OnClickListener for the "Send" button.
-         * In your implementation you need to get the message from the input box (EditText)
-         * and send it to other AVDs.
+        
+        /* 
+         *  Registering and implementing an OnClickListener for the "Send" button.
+         *  Getting the message from the input box (EditText) and sending it to other AVDs.
+         *  Creating editText to display
          */
-        // So lets create editText to display
         final EditText editText = (EditText) findViewById(R.id.editText1);
         findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,8 +106,9 @@ public class GroupMessengerActivity extends Activity {
                     localTextView.append("\t" + msg); // This is one way to display a string.
                     TextView remoteTextView = (TextView) findViewById(R.id.textView1);
                     remoteTextView.append("\n");
-
-                        new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
+                    
+                    // Client task invocation
+                    new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
 
             }
         });
@@ -115,7 +121,10 @@ public class GroupMessengerActivity extends Activity {
         return true;
     }
 
-
+    // ---------------------------------------------------------------------
+    // Server Task Implementation
+    // Receives messages and passes them to onProgressUpdate()
+    // ---------------------------------------------------------------------
     private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
 
         @Override
@@ -126,8 +135,12 @@ public class GroupMessengerActivity extends Activity {
             while(true) {
                 try {
                     server = serverSocket.accept();
+                    
+                    // InputStream
                     InputStream is = server.getInputStream();
                     BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    
+                    // OutputStream
                     String msgToSend = "ACK MSG";
                     DataOutputStream outToServer = new DataOutputStream(server.getOutputStream());
                     outToServer.writeBytes(msgToSend);
@@ -137,31 +150,28 @@ public class GroupMessengerActivity extends Activity {
                     Log.e(TAG, "publish progress failed");
                 }
             }
-            /*
-             * TODO: Fill in your server code that receives messages and passes them
-             * to onProgressUpdate().
-             */
-            // return null;
         }
-
+ 
+        // ---------------------------------------------------------------------
+        // onProgressUpdate()
+        // Displays what is received in doInBackground().
+        // --------------------------------------------------------------------- 
         protected void onProgressUpdate(String...strings) {
-            /*
-             * The following code displays what is received in doInBackground().
-             */
             String strReceived = strings[0].trim();
             TextView remoteTextView = (TextView) findViewById(R.id.textView1);
             remoteTextView.append(strReceived + "\t\n");
             TextView localTextView = (TextView) findViewById(R.id.textView1);
             localTextView.append("\n");
-            // insert into database
-            // Need to create Uri object and contentValue to insert
-
-            //Building URI object
+            
+            // Need to build Uri object and ContentValues to insert the message into the file system
+            // Building URI object
             Uri uri = Uri.parse("content://edu.buffalo.cse.cse486586.groupmessenger1.provider");
-            // Building ContentValues Object
+            
+            // Building ContentValues Object with key as sequence number and value as the msg received
             ContentValues contVal = new ContentValues();
             contVal.put(KEY_FIELD, Integer.toString(sequenceNum));
             contVal.put(VALUE_FIELD,strReceived);
+            
             // Inserting
             getContentResolver().insert(uri, contVal);
             sequenceNum++;
@@ -169,30 +179,33 @@ public class GroupMessengerActivity extends Activity {
         }
     }
 
-    /***
-     * ClientTask is an AsyncTask that should send a string over the network.
-     * It is created by ClientTask.executeOnExecutor() call whenever OnKeyListener.onKey() detects
-     * an enter key press event.
-     *
-     * @author stevko
-     *
-     */
+    // ---------------------------------------------------------------
+    // ClientTask is an AsyncTask that should send a string over the
+    // network. It is created by ClientTask.executeOnExecutor() call 
+    // whenever OnKeyListener.onKey() detects an enter key press event.
+    // ---------------------------------------------------------------
     private class ClientTask extends AsyncTask<String, Void, Void> {
 
-        @Override
+        // -------------------------------------------
+        // @Override - doInBackground()
+        // Method to send messages to all the AVD's 
+        // including myself
+        // -------------------------------------------
         protected Void doInBackground(String... msgs) {
             try {
-                // So need to send messages for all of the AVD's including myself
-                // The below code has been taken from my PA1
+                
                 String[] remotePorts = {REMOTE_PORT0, REMOTE_PORT1, REMOTE_PORT2, REMOTE_PORT3, REMOTE_PORT4};
                 for(int i = 0; i < 5; i++) {
                     Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
                             Integer.parseInt(remotePorts[i]));
-
+                    
+                    // OutputStream
                     String msgToSend = msgs[0];
                     DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
                     outToServer.writeBytes(msgToSend);
                     outToServer.flush();
+                    
+                    // InputStream
                     InputStream is = socket.getInputStream();
                     BufferedReader br = new BufferedReader(new InputStreamReader(is));
                     if (br.readLine().equals("ACK MSG")) {
